847b8a93fe63ba1c5fc02571ab4045fb7ba75965
qrxnz <111581337+qrxnz@users.noreply.github.com>
2023-10-25 22:22:42 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,147 @@
+--                        _           
+--                       (_)          
+--  _ __   ___  _____   ___ _ __ ___  
+-- | '_ \ / _ \/ _ \ \ / / | '_ ` _ \ 
+-- | | | |  __/ (_) \ V /| | | | | | |
+-- |_| |_|\___|\___/ \_/ |_|_| |_| |_|
+                                    
+                                    
+-- vim settings
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.termguicolors = true
+vim.opt.smarttab = false
+vim.opt.hlsearch = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- lazy nvim(plugin manager)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  "nvim-lualine/lualine.nvim",
+  "nvim-tree/nvim-web-devicons",
+   {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    "mfussenegger/nvim-dap",
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
+    "nvim-treesitter/nvim-treesitter"
+
+})
+
+-- colorscheme
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false, -- disables setting the background color.
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+    },
+})
+
+vim.cmd.colorscheme "catppuccin"
+
+-- status line
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'catppuccin',
+    component_separators = '|',
+    section_separators = '',
+  },
+}
+
+-- Telescope
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
+-- mason
+require("mason").setup()
