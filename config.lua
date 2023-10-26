bbc3b28a979f8e5f4cae7c4741ba0d4610670596
qrxnz <111581337+qrxnz@users.noreply.github.com>
2023-10-26 12:27:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -145,3 +145,24 @@ require('telescope').setup{
 
 -- mason
 require("mason").setup()
+
+-- dap
+local dap = require("dap")
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "-i", "dap" }
+}
+
+local dap = require("dap")
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = "${workspaceFolder}",
+  },
+}
