555b41b50db572e3292de555f66218e653cdbb5d
Karol Skoczyk <111581337+qrxnz@users.noreply.github.com>
2023-11-28 22:13:09 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,6 +11,7 @@ vim.opt.number = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.termguicolors = true
 vim.opt.smarttab = false
 vim.opt.hlsearch = true
@@ -50,8 +51,12 @@ require("lazy").setup({
     "williamboman/mason.nvim",
     "neovim/nvim-lspconfig",
     "williamboman/mason-lspconfig.nvim",
-    "nvim-treesitter/nvim-treesitter"
-
+    "nvim-treesitter/nvim-treesitter",
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lsp',
+    'rafamadriz/friendly-snippets' 
 })
 
 -- colorscheme
@@ -147,6 +152,57 @@ require('telescope').setup{
 
 -- mason
 require("mason").setup()
+
+-- cmp
+
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
 -- dap
 local dap = require("dap")
